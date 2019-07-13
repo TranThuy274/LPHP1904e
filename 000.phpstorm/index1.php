396183a447f6360aad85d92bf9c3e5b3e4663e91
TranThuy274 <tranthuthuy274@gmail.com>
2019-07-13 18:48:05 +0700

    php

---
+++ 000.phpstorm/index1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+<body>
+<pre>
+    Các
+    1. ctrl f: tìm kiếm trong file hiện tại
+    2. ctrl r: tìm kiếm và thay thế cụm từ trong file hiện tại
+    3. ctrl shift f: tìm kiếm trong toàn bộ dự án
+    4. ctrl shift r: tìm kiếm thay thế 1 chuỗi trong toàn bộ project
+    5. shift shift: tìm kiếm every where trong toàn bộ dự án
+    6. shift f6: rename file
+
+</pre>
+</body>
+</html>
