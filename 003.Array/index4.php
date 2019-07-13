dc5d8e988639280f704fc22bc76c2bf0f0e3d684
TranThuy274 <tranthuthuy274@gmail.com>
2019-07-13 18:30:33 +0700

    php

--- 003.Array/index4.php
+++ 003.Array/index4.php
@@ -1,7 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+<body>
 <?php
+//cách khai bảo mảng kiểu 1
+
 /**
- * Created by PhpStorm.
- * User: Park Park
- * Date: 7/8/2019
- * Time: 8:08 PM
+ * áp dụng cho mảng chỉ số
+ * mảng chí số sẽ tự tạo key khi truyền value của phần tử
  */
+
+$cities = array("hà nội", "hải phòng", "nghệ an", "hà tĩnh");
+
+//cách số 2
+/**
+ * Khai bảo key cho mảng
+ */
+$cities = array(0 => "hà nội", 1 => "thanh hoá", 2 => "nghệ an", 3 => "hà tĩnh", 4 => "hồ chí minh");
+// cách số 3
+$cities = array();
+$cities[] = "hà nội";
+$cities[] = "thanh hoá";
+$cities[] = "nghệ an";
+$cities[] = "hà tĩnh";
+$cities[] = "hồ chí minh";
+// cách số 4
+$cities = array();
+$cities[0] = "hà nội";
+$cities[1] = "thanh hoá";
+$cities[2] = "nghệ an";
+$cities[3] = "hà tĩnh";
+$cities[4] = "hồ chí minh";
+?>
+</body>
+</html>
+
+
+?>
+</body>
+</html>
---
+++ 003.Array/index5.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+</head>
+<body>
+<pre>
+    Mảng kết hợp là mảng mà key là 1 chuỗi và số hoặc chuỗi
+</pre>
+
+<?php
+// cách 1
+$array2 = array();
+$array2["hn"] = "hà nội";
+$array2["th"] = "thanh hoá";
+$array2["na"] = "nghệ an";
+$array2["ht"] = "hà tĩnh";
+$array2["hc"] = "hồ chí minh";
+$array2[12] = "long an";
+// cách 2
+$array2 = array("hn" => "hà nội", "th" => "thanh hoá", "na" => "nghệ an");
+echo "<pre>";
+print_r($array2);
+echo "</pre>";
+?>
+</body>
+</html>
